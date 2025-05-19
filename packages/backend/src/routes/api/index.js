81ab7a616cd793144697ba8101785e263d0b5ea8
Ali BARIN <ali.barin53@gmail.com>
2025-05-19 14:27:48 +0000

    fix(cors): allow CORS for all origins for openapi specs

--- packages/backend/src/routes/api/index.js
+++ packages/backend/src/routes/api/index.js
@@ -1,12 +1,13 @@
+import cors from 'cors';
 import { Router } from 'express';
-import v1Router from './v1/index.ee.js';
-import { checkIsEnterprise } from '../../helpers/check-is-enterprise.js';
 import { authenticateApiToken } from '../../helpers/authenticate-api-token.ee.js';
+import { checkIsEnterprise } from '../../helpers/check-is-enterprise.js';
 import renderOpenApiJson from '../../helpers/render-openapi-json.ee.js';
+import v1Router from './v1/index.ee.js';
 
 const router = Router();
 
 router.use('/v1', checkIsEnterprise, authenticateApiToken, v1Router);
-router.get('/openapi.json', renderOpenApiJson);
+router.get('/openapi.json', cors({ origin: '*' }), renderOpenApiJson);
 
 export default router;
